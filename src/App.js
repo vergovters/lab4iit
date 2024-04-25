284174e3999c772fa9e57c75b04a5296c4234788
illia <fialkivskiji@gmail.com>
2024-04-25 14:04:07 +0300

    912

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React 91
+          Learn React 912
         </a>
       </header>
     </div>
